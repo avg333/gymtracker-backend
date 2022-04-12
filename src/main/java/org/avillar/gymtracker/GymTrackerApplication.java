670b9235694c10f4d25145e7b675cee3811f4c19
Adrian <avg333@hotmail.com>
2022-04-12 13:34:06 +0200

    commit

--- src/main/java/org/avillar/gymtracker/GymTrackerApplication.java
+++ src/main/java/org/avillar/gymtracker/GymTrackerApplication.java
@@ -4,8 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class GymTrackerApplication {
 
     public static void main(String[] args) {
