7693f4fb8ad171e0a9c2eb25cb62a06ce08d27bf
Adrian <avg333@hotmail.com>
2022-04-15 01:01:38 +0200

    commit

--- src/main/java/org/avillar/gymtracker/api/controllers/ViewsController.java
+++ src/main/java/org/avillar/gymtracker/api/controllers/ViewsController.java
@@ -1,11 +1,19 @@
 package org.avillar.gymtracker.api.controllers;
 
+import org.avillar.gymtracker.api.dto.ProgramDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ViewsController {
+
+    @Autowired
+    private ProgramController programController;
 
     @GetMapping("/")
     @ResponseBody
@@ -26,6 +34,12 @@ public class ViewsController {
     @GetMapping("/programs")
     public String programsPage() {
         return "programs";
+    }
+
+    @PostMapping("/programs/createProgram")
+    public RedirectView addProgram(@ModelAttribute("programDto") ProgramDto programDto ){
+        programController.createProgram(programDto);
+        return new RedirectView("");
     }
 
 }
