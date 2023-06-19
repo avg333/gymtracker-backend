cec2b4d988bbf267c3b192a0d293c6fcf2d7562d
Adrián Villar <avg333@hotmail.com>
2023-06-19 15:11:20 +0200

    commit

--- src/main/java/org/avillar/gymtracker/common/auditing/JpaConfig.java
+++ src/main/java/org/avillar/gymtracker/common/auditing/JpaConfig.java
@@ -10,8 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @RequiredArgsConstructor
 public class JpaConfig {
-    @Bean
-    public AuditorAware<String> auditorAware() {
-        return new AuditorAwareImpl();
-    }
+  @Bean
+  public AuditorAware<String> auditorAware() {
+    return new AuditorAwareImpl();
+  }
 }
---
+++ src/test/java/org/avillar/gymtracker/workoutapi/set/infrastructure/post/PostSetControllerTest.java
@@ -0,0 +1,74 @@
+package org.avillar.gymtracker.workoutapi.set.infrastructure.post;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+import org.avillar.gymtracker.workoutapi.set.application.post.PostSetService;
+import org.avillar.gymtracker.workoutapi.set.application.post.model.PostSetRequestApplication;
+import org.avillar.gymtracker.workoutapi.set.application.post.model.PostSetResponseApplication;
+import org.avillar.gymtracker.workoutapi.set.application.post.model.PostSetResponseApplication.SetGroup;
+import org.avillar.gymtracker.workoutapi.set.infrastructure.post.mapper.PostSetControllerMapper;
+import org.avillar.gymtracker.workoutapi.set.infrastructure.post.model.PostSetRequestInfrastructure;
+import org.avillar.gymtracker.workoutapi.set.infrastructure.post.model.PostSetResponseInfrastructure;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PostSetControllerTest {
+
+  @InjectMocks private PostSetController postSetController;
+
+  @Mock private PostSetService postSetService;
+
+  @Mock private PostSetControllerMapper postSetControllerMapper; // TODO No mockear el mapper
+
+  @Test
+  void postSet() {
+    final UUID setGroupId = UUID.randomUUID();
+    final String description = "Description example 54.";
+    final double weight = 115.2;
+    final double rir = 8.0;
+    final int reps = 6;
+    final PostSetRequestInfrastructure postSetRequestInfrastructure =
+        new PostSetRequestInfrastructure();
+    postSetRequestInfrastructure.setReps(reps);
+    postSetRequestInfrastructure.setRir(rir);
+    postSetRequestInfrastructure.setWeight(weight);
+    postSetRequestInfrastructure.setDescription(description);
+
+    final UUID setId = UUID.randomUUID();
+    final int listOrder = 0;
+    when(postSetService.post(eq(setGroupId), any(PostSetRequestApplication.class)))
+        .thenReturn(
+            new PostSetResponseApplication(
+                setId, listOrder, description, reps, rir, weight, new SetGroup(setGroupId)));
+    when(postSetControllerMapper.postRequest(postSetRequestInfrastructure))
+        .thenReturn(new PostSetRequestApplication());
+    when(postSetControllerMapper.postResponse(any(PostSetResponseApplication.class)))
+        .thenReturn(
+            new PostSetResponseInfrastructure(
+                setId,
+                listOrder,
+                description,
+                reps,
+                rir,
+                weight,
+                new PostSetResponseInfrastructure.SetGroup(setGroupId)));
+
+    final PostSetResponseInfrastructure postSetResponseInfrastructure =
+        postSetController.postSet(setGroupId, postSetRequestInfrastructure).getBody();
+    Assertions.assertEquals(setId, postSetResponseInfrastructure.getId());
+    Assertions.assertEquals(listOrder, postSetResponseInfrastructure.getListOrder());
+    Assertions.assertEquals(description, postSetResponseInfrastructure.getDescription());
+    Assertions.assertEquals(rir, postSetResponseInfrastructure.getRir());
+    Assertions.assertEquals(reps, postSetResponseInfrastructure.getReps());
+    Assertions.assertEquals(weight, postSetResponseInfrastructure.getWeight());
+    Assertions.assertEquals(setGroupId, postSetResponseInfrastructure.getSetGroup().getId());
+  }
+}
