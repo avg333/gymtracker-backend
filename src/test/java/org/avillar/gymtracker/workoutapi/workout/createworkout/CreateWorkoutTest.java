1a88c656c511697cdf39e0540568d9bf73665141
Adrián Villar <avg333@hotmail.com>
2023-06-29 13:31:49 +0200

    commit

--- src/test/java/org/avillar/gymtracker/workoutapi/workout/createworkout/CreateWorkoutTest.java
+++ src/test/java/org/avillar/gymtracker/workoutapi/workout/createworkout/CreateWorkoutTest.java
@@ -1,19 +1,22 @@
 package org.avillar.gymtracker.workoutapi.workout.createworkout;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
 import org.avillar.gymtracker.authapi.auth.domain.UserApp;
 import org.avillar.gymtracker.authapi.auth.domain.UserDao;
+import org.avillar.gymtracker.workoutapi.domain.SetGroupDao;
 import org.avillar.gymtracker.workoutapi.domain.Workout;
 import org.avillar.gymtracker.workoutapi.domain.WorkoutDao;
+import org.avillar.gymtracker.workoutapi.workout.createworkout.infrastructure.model.CreateWorkoutResponseInfrastructure;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,82 +32,121 @@ class CreateWorkoutTest {
 
   private static final String USER_NAME_OK = "adrian";
   private static final String USER_NAME_KO = "chema";
-  final List<Workout> workouts = new ArrayList<>();
   @Autowired private MockMvc mockMvc;
   @Autowired private WorkoutDao workoutDao;
+  @Autowired private SetGroupDao setGroupDao;
   @Autowired private UserDao userDao;
 
   @AfterEach
   void afterEach() {
-    var works = workoutDao.findAll();
     workoutDao.deleteAll();
-    if (!works.isEmpty()) workoutDao.deleteById(works.get(0).getId());
-    workouts.clear();
   }
 
   @Test
   @WithUserDetails(USER_NAME_OK)
   void createOneWorkoutOk() throws Exception {
     final UserApp userApp = userDao.findByUsername(USER_NAME_OK);
-    final String updateWorkoutDateRequest =
-        """
-{
-"date": "%s",
-"description": "Description TEST"
-}
-""";
+    final JSONObject updateWorkoutDateRequest = new JSONObject();
+    updateWorkoutDateRequest.put("date", "2022-03-03");
+    updateWorkoutDateRequest.put("description", "Description TEST");
+
+    final ResultActions resultActions =
+        mockMvc
+            .perform(
+                post("/workout-api/users/" + userApp.getId() + "/workouts")
+                    .contentType(APPLICATION_JSON)
+                    .content(updateWorkoutDateRequest.toString()))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.userId").value(userApp.getId().toString()))
+            .andExpect(jsonPath("$.date").value(updateWorkoutDateRequest.get("date")))
+            .andExpect(
+                jsonPath("$.description").value(updateWorkoutDateRequest.get("description")));
+
+    final CreateWorkoutResponseInfrastructure result =
+        new ObjectMapper()
+            .readValue(
+                resultActions.andReturn().getResponse().getContentAsString(),
+                CreateWorkoutResponseInfrastructure.class);
+
+    final Optional<Workout> workoutDb = workoutDao.findById(result.getId());
+    assertTrue(workoutDb.isPresent());
+    assertEquals(updateWorkoutDateRequest.get("description"), result.getDescription());
+    // assertEquals(updateWorkoutDateRequest.get("date"), result.getDate());
+    // TODO Verificar como comprobar este date
+    assertEquals(userApp.getId(), result.getUserId());
+    assertTrue(setGroupDao.getSetGroupsByWorkoutId(result.getId()).isEmpty());
+  }
+
+  @Test
+  @WithUserDetails(USER_NAME_OK)
+  void createTwoWorkoutsInTheSameDate() throws Exception {
+    final UserApp userApp = userDao.findByUsername(USER_NAME_OK);
+    final JSONObject updateWorkoutDateRequest = new JSONObject();
+    updateWorkoutDateRequest.put("date", "2022-03-03");
+    updateWorkoutDateRequest.put("description", "Description TEST");
 
     mockMvc
         .perform(
             post("/workout-api/users/" + userApp.getId() + "/workouts")
                 .contentType(APPLICATION_JSON)
-                .content(String.format(updateWorkoutDateRequest, "2023-06-28")))
+                .content(updateWorkoutDateRequest.toString()))
         .andDo(print())
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.date").value("2023-06-28"));
-
-    // final Optional<Workout> workoutDb = workoutDao.findById(workoutId);
-    // Assertions.assertTrue(workoutDb.isPresent());
-    // Assertions.assertEquals("newDescription", workoutDb.get().getDescription());
-  }
-
-  @Test
-  @WithUserDetails(USER_NAME_OK)
-  void deleteOneWorkout2() throws Exception {
-    final Workout workout = workouts.get(0);
-    final String updateWorkoutDateRequest = """
-{"description": null}
-""";
+        .andExpect(jsonPath("$.userId").value(userApp.getId().toString()))
+        .andExpect(jsonPath("$.date").value(updateWorkoutDateRequest.get("date")))
+        .andExpect(jsonPath("$.description").value(updateWorkoutDateRequest.get("description")));
 
     mockMvc
         .perform(
-            patch("/workout-api/workouts/" + workout.getId() + "/date")
+            post("/workout-api/users/" + userApp.getId() + "/workouts")
                 .contentType(APPLICATION_JSON)
-                .content(updateWorkoutDateRequest))
+                .content(updateWorkoutDateRequest.toString()))
         .andDo(print())
         .andExpect(status().isBadRequest());
   }
 
   @Test
   @WithUserDetails(USER_NAME_KO)
-  void deleteNotFoundAndNotPermission() throws Exception {
-    final Workout workout = workouts.get(0);
-    final String updateWorkoutDateRequest = """
-{"description": "newDescription"}
-""";
+  void createOneInOtherUser() throws Exception {
+    final UserApp userApp = userDao.findByUsername(USER_NAME_OK);
+    final JSONObject updateWorkoutDateRequest = new JSONObject();
+    updateWorkoutDateRequest.put("date", "2022-03-03");
+    updateWorkoutDateRequest.put("description", "Description TEST");
+
     mockMvc
         .perform(
-            patch("/workout-api/workouts/" + UUID.randomUUID() + "/description")
+            post("/workout-api/users/" + userApp.getId() + "/workouts")
                 .contentType(APPLICATION_JSON)
-                .content(updateWorkoutDateRequest))
-        .andDo(print())
-        .andExpect(status().isNotFound());
-    mockMvc
-        .perform(
-            patch("/workout-api/workouts/" + workout.getId() + "/description")
-                .contentType(APPLICATION_JSON)
-                .content(updateWorkoutDateRequest))
+                .content(updateWorkoutDateRequest.toString()))
         .andDo(print())
         .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithUserDetails(USER_NAME_OK)
+  void createOneWorkoutBadRequest() throws Exception {
+    final UserApp userApp = userDao.findByUsername(USER_NAME_OK);
+
+    final JSONObject updateWorkoutDateRequest = new JSONObject();
+    updateWorkoutDateRequest.put("date", null);
+    updateWorkoutDateRequest.put("description", "Description TEST");
+    mockMvc
+        .perform(
+            post("/workout-api/users/" + userApp.getId() + "/workouts")
+                .contentType(APPLICATION_JSON)
+                .content(updateWorkoutDateRequest.toString()))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
+
+    updateWorkoutDateRequest.put("date", "2022-03-03");
+    updateWorkoutDateRequest.put("description", new String(new char[300]).replace('\0', ' '));
+    mockMvc
+        .perform(
+            post("/workout-api/users/" + userApp.getId() + "/workouts")
+                .contentType(APPLICATION_JSON)
+                .content(updateWorkoutDateRequest.toString()))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
   }
 }
