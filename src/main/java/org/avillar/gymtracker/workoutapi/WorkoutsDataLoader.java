e7d73bf5e6ddd52bc6c1f94572f9a3a9447fe564
Adrián Villar <avg333@hotmail.com>
2023-07-10 23:32:22 +0200

    commit

--- src/main/java/org/avillar/gymtracker/workoutapi/WorkoutsDataLoader.java
+++ src/main/java/org/avillar/gymtracker/workoutapi/WorkoutsDataLoader.java
@@ -1,6 +1,7 @@
 package org.avillar.gymtracker.workoutapi;
 
 import java.util.*;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.avillar.gymtracker.authapi.domain.UserApp;
@@ -28,12 +29,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "${workoutsApiPrefix}/")
 public class WorkoutsDataLoader implements ApplicationRunner {
 
-  private static final int TOTAL_USERS = 5;
   private static final int TOTAL_DIAS = 365;
   private static final double PROB_GO_TO_THE_GIM = 0.7;
   private static final int MIN_EXERCISES = 3;
   private static final int MAX_EXERCISES = 6;
-  private static final int MAX_INSERTS = 250000;
   private final Random random = new Random();
   private final UserDao userDao;
   private final ExerciseDao exerciseDao;
@@ -41,16 +40,14 @@ public class WorkoutsDataLoader implements ApplicationRunner {
   private final SetGroupDao setGroupDao;
   private final SetDao setDao;
   private final WorkoutDao workoutDao;
-  private final List<Workout> workouts = new ArrayList<>();
-  private final List<SetGroup> setGroups = new ArrayList<>();
-  private final List<Set> sets = new ArrayList<>();
+
 
   @Value("${spring.profiles.active}")
   private String activeProfile;
 
   @PostMapping("/users/{userId}/create")
   public ResponseEntity<Void> postWorkout(@PathVariable final UUID userId) {
-    this.createAll(userId);
+    this.createHeavyData(List.of(userId));
     return ResponseEntity.noContent().build();
   }
 
@@ -64,48 +61,43 @@ public class WorkoutsDataLoader implements ApplicationRunner {
     }
 
     log.info("Populating workouts micro...");
-    createHeavyData();
-    saveHeavyData();
-    int totalInserts = workouts.size() + setGroups.size() + sets.size();
+    var userIds = userDao.findAll().stream().map(UserApp::getId).toList();
+    final SaveData saveData = createHeavyData(userIds);
+    saveHeavyData(saveData);
+    int totalInserts =
+        saveData.getWorkouts().size() + saveData.getSetGroups().size() + saveData.getSets().size();
 
     long finish = System.currentTimeMillis();
     log.info("Populated workouts micro with {} entities in {} ms", totalInserts, finish - start);
   }
 
-  private void saveHeavyData() {
-    log.info("\tInserting " + workouts.size() + " workouts...");
-    workoutDao.saveAll(workouts);
-    log.info("\tInserted " + workouts.size() + " workouts");
+  private void saveHeavyData(final SaveData saveData) {
+    log.info("\tInserting {} workouts...", saveData.getWorkouts().size());
+    workoutDao.saveAll(saveData.getWorkouts());
+    log.info("\tInserted {} workouts", saveData.getWorkouts().size());
 
-    log.info("\tInserting " + setGroups.size() + " setGroups...");
-    setGroupDao.saveAll(setGroups);
-    log.info("\tInserted " + setGroups.size() + " setGroups");
+    log.info("\tInserting {} setGroups...", saveData.getSetGroups().size());
+    setGroupDao.saveAll(saveData.getSetGroups());
+    log.info("\tInserted {} setGroups", saveData.getSetGroups().size());
 
-    log.info("\tInserting " + sets.size() + " sets...");
-    for (int i = 0; i < sets.size(); i += MAX_INSERTS) {
-      saveSets(sets, i, i + MAX_INSERTS);
-    }
-    log.info("\tInserted " + sets.size() + " sets");
+    log.info("\tInserting {} sets...", saveData.getSets().size());
+    setDao.saveAll(saveData.getSets());
+    log.info("\tInserted {} sets", saveData.getSets().size());
   }
 
-  private void saveSets(final List<Set> sets, int min, int max) {
-    log.info("\t\tInserting(sub) " + sets.size() + " sets...");
-    setDao.saveAll(sets.subList(min, Math.min(sets.size(), max)));
-    log.info("\t\tInserted(sub) " + sets.size() + " sets");
-  }
-
-  private void createHeavyData() {
-    var userIds = userDao.findAll().stream().map(UserApp::getId).toList();
+  private SaveData createHeavyData(final List<UUID> userIds) {
+    final SaveData saveData = new SaveData();
     exerciseIds.addAll(exerciseDao.findAll().stream().map(Exercise::getId).toList());
-    // var userIds = createUsers();
-    log.info("\tCreating " + TOTAL_USERS + " users...");
+    log.info("\tCreating workouts for {} users...", userIds.size());
     for (var userId : userIds) {
-      createWorkoutsForUser(userId);
+      createWorkoutsForUser(userId, saveData);
     }
-    log.info("\tCreated " + TOTAL_USERS + " users");
+    log.info("\tCreated {} users", userIds.size());
+
+    return saveData;
   }
 
-  private void createWorkoutsForUser(UUID userId) {
+  private void createWorkoutsForUser(UUID userId, SaveData saveData) {
     final int days = 35;
     final Calendar c = Calendar.getInstance();
     c.setTime(new Date());
@@ -113,103 +105,47 @@ public class WorkoutsDataLoader implements ApplicationRunner {
     for (int i = 0; i < TOTAL_DIAS; i++) {
       c.add(Calendar.DATE, 1);
       if (random.nextDouble() < PROB_GO_TO_THE_GIM) {
-        createWorkout(userId, c.getTime());
+        createWorkout(userId, c.getTime(), saveData);
       }
     }
   }
 
-  private void createWorkout(UUID userId, Date date) {
+  private void createWorkout(UUID userId, Date date, SaveData saveData) {
     var workout = new Workout(date, "", userId, new HashSet<>());
     int totalExercises = MIN_EXERCISES + random.nextInt(MAX_EXERCISES - MIN_EXERCISES + 1);
-    workouts.add(workout);
+    saveData.getWorkouts().add(workout);
     for (int i = 0; i < totalExercises; i++) {
-      createSetGroup(workout, i);
+      createSetGroup(workout, i, saveData);
     }
   }
 
-  private void createSetGroup(Workout workout, int listOrder) {
-    var setGroup =
-        new SetGroup(
-            null, exerciseIds.get(random.nextInt(exerciseIds.size())), workout, new HashSet<>());
+  private void createSetGroup(Workout workout, int listOrder, SaveData saveData) {
+    UUID exerciseId =
+        exerciseIds.isEmpty()
+            ? UUID.randomUUID()
+            : exerciseIds.get(random.nextInt(exerciseIds.size()));
+    var setGroup = new SetGroup(null, exerciseId, workout, new HashSet<>());
     setGroup.setListOrder(listOrder);
     int totalSets = MIN_EXERCISES + random.nextInt(MAX_EXERCISES - MIN_EXERCISES + 1);
-    setGroups.add(setGroup);
+    saveData.getSetGroups().add(setGroup);
     for (int i = 0; i < totalSets; i++) {
-      createSets(setGroup, i);
+      createSet(setGroup, i, saveData);
     }
   }
 
-  private void createSets(SetGroup setGroup, int listOrder) {
-    var set = new Set(null, 12, 12.0, 12.0, setGroup);
+  private void createSet(SetGroup setGroup, int listOrder, SaveData saveData) {
+    int reps = 3 + random.nextInt(18 - 3 + 1);
+    int rir = random.nextInt(3 + 1);
+    int weight = 10 + random.nextInt(150 - 10 + 1);
+    var set = new Set(null, reps, (double) rir, (double) weight, setGroup);
     set.setListOrder(listOrder);
-    sets.add(set);
+    saveData.getSets().add(set);
   }
 
-  private List<UUID> createUsers() {
-    final List<UUID> userIds = new ArrayList<>();
-    for (int i = 0; i < TOTAL_USERS; i++) {
-      userIds.add(UUID.randomUUID());
-    }
-
-    return userIds;
-  }
-
-  public void createAll(UUID uuid) {
-    createWorkouts(uuid);
-    createSets();
-  }
-
-  private void createWorkouts(final UUID userId) {
-    final List<Workout> workouts = new ArrayList<>();
-    final int days = 35;
-    final Calendar c = Calendar.getInstance();
-    c.setTime(new Date());
-    c.add(Calendar.DATE, -days - 1);
-    for (int i = 0; i < days; i++) {
-      c.add(Calendar.DATE, 1);
-      if (random.nextDouble() < 0.2) {
-        continue;
-      }
-      workouts.add(new Workout(c.getTime(), null, userId, null));
-    }
-    workoutDao.saveAll(workouts);
-  }
-
-  private void createSets() {
-    final List<Workout> workouts = workoutDao.findAll();
-    final List<SetGroup> setGroups = new ArrayList<>();
-
-    for (final Workout workout : workouts) {
-      for (int i = 0; 5 > i; i++) {
-        long rnd = random.nextLong();
-        final SetGroup setGroup =
-            new SetGroup(
-                random.nextDouble() < 0.2 ? "WorkoutSetGroup" + rnd : null,
-                exerciseIds.get(random.nextInt(exerciseIds.size())),
-                workout,
-                null);
-        setGroup.setListOrder(i);
-        setGroups.add(setGroup);
-      }
-    }
-    setGroupDao.saveAll(setGroups);
-
-    final List<Set> sets = new ArrayList<>();
-    for (final SetGroup setGroup : setGroups) {
-      final int totalSeries = random.nextInt(2, 6);
-      for (int i = 0; i < totalSeries; i++) {
-        final int reps = random.nextInt(3, 15);
-        final double rir = random.nextInt(0, 4);
-        final double weight = Math.round((random.nextInt(5, 100)) * 100.0) / 100.0;
-        final Set set =
-            new Set(
-                random.nextDouble() < 0.2 ? "SetDescription" : null, reps, rir, weight, setGroup);
-        set.setListOrder(i);
-        sets.add(set);
-      }
-    }
-
-    setDao.saveAll(sets);
-    log.info("Creados las sets");
+  @Data
+  public static class SaveData {
+    private final List<Workout> workouts = new ArrayList<>();
+    private final List<SetGroup> setGroups = new ArrayList<>();
+    private final List<Set> sets = new ArrayList<>();
   }
 }
